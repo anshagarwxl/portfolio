4d10bd4d693e8a8a105fa6c5ac4bd03c25dd6857
ansh <agansh06@gmail.com>
2025-10-01 20:12:31 +0530

    removed achievements tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/home/hero";
 import { FeaturedProjects } from "@/components/home/featured-projects";
 import { SkillsShowcase } from "@/components/home/skills-showcase";
-import { Achievements } from "@/components/home/achievements";
+// import { Achievements } from "@/components/home/achievements";
 //import { Testimonials } from "@/components/home/testimonials";
 
 export default function HomePage() {
@@ -10,7 +10,6 @@ export default function HomePage() {
       <Hero />
       <FeaturedProjects />
       <SkillsShowcase />
-      <Achievements />
     </div>
   );
 }
